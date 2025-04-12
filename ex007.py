bcc3d24cc4631bbe0bdf696b5b8bd7675cbd50b3
AgathaYasminn666 <a.y.anjos@uni9.edu.br>
2025-04-12 16:12:25 -0300

    Create ex007.py
    média aritmética

---
+++ ex007.py
@@ -0,0 +1,6 @@
+#média artmética
+
+n1 = float(input('Preimeira nota: '))
+n2 = float(input('Segunda nota: '))
+média = (n1+n2) /2
+print('A média entre {:.1f} e {:.1f} é {:.1f}'.format(n1, n2, média))
