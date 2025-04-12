c61a6240e7e21bf88d75cf199c1b869107ab824c
AgathaYasminn666 <a.y.anjos@uni9.edu.br>
2025-04-12 11:44:12 -0300

    Create ex002.py
    calculadora de soma

---
+++ ex002.py
@@ -0,0 +1,13 @@
+#jeito 1
+n1 = int( input ('digite um número '))
+print (type(n1))
+n2 = int( input('digite mais um número '))
+s = n1+n2
+print (type(n2))
+print('a soma entre {} e {} vale {} '.format(n1, n2, s))
+
+#jeito 2 (mais facíl)
+n1 = int( input ('digite um número '))
+n2 = int( input('digite mais um número '))
+s = n1+n2
+print ('a soma entre os valores vale {}' .format(s))
