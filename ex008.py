5d13d9d9caa42b265b6146eb01c1b4102bb0b1c1
AgathaYasminn666 <a.y.anjos@uni9.edu.br>
2025-04-12 16:35:22 -0300

    Create ex008.py
    medição de distância

---
+++ ex008.py
@@ -0,0 +1,6 @@
+#medição de distância
+
+medida = float(input('Uma distância em metros: '))
+cm = medida * 100
+mm = medida * 1000
+print('A medida de {}m corresponde a {}cm e {}mm'.format(medida, cm, mm))
