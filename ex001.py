9a0c06c6cc5f8b729ed7cc37113b8dbcb8c607c2
AgathaYasminn666 <a.y.anjos@uni9.edu.br>
2025-04-12 11:42:35 -0300

    Create ex001.py
    boas vindas, nome+idade+peso, data de nascimento

---
+++ ex001.py
@@ -0,0 +1,18 @@
+nome = input('Qual o seu nome?')
+print('bem vinda', nome)
+
+nome = input ('qual o seu nome?')
+idade = input ('qual a sua idade?')
+peso = input ('qual o seu peso?' )
+print (nome, idade, peso)
+
+nome = input ('qual sua data de nascimeto?')
+mês = input ('qual o seu mês de nascimento?')
+ano = input ('qual o seu ano de nascimento?')
+print (nome, mês, ano)
+
+dia = input ('Qual sua data de nascimento?')
+mes = input ('Qual o mês de seu nascimento?')
+ano = input ('Qual o seu ano de nascimento?')
+frase = input ('Você nasceu dia 28 de janeiro de 2007')
+print (dia, mes, ano, frase)
